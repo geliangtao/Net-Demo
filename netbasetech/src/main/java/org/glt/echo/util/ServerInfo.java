064be057eb5dcaba441abe7cdca6fefeec7baabd
geliangtao <emailglt@163.com>
2019-11-13 21:41:23 +0800

    BIO/NIO/AIO ECHO DEMO

---
+++ netbasetech/src/main/java/org/glt/echo/util/ServerInfo.java
@@ -0,0 +1,10 @@
+package org.glt.echo.util;
+
+public class ServerInfo {
+
+    //主机名
+    public static final String HOST_NAME = "localhost" ;
+    //端口
+    public static final int PORT = 9999;
+
+}
